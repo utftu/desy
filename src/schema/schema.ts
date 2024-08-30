a57802eaf86df883d67b81173ed02bffe6dca873
utftufutukgyftryidytftuv <overby999@gmail.com>
2024-08-30 16:29:56 +0300

    30.08.24 16:29

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -6,6 +6,7 @@ type Config = {
 };
 
 export type Infer<TType extends Schema<any>> = DeepCopy<TType['types']>;
+type ConfigValidate = {path: string};
 
 export abstract class Schema<TValue> {
   types!: TValue;
@@ -14,7 +15,7 @@ export abstract class Schema<TValue> {
   constructor({context}: Config) {
     this.context = context;
   }
-  validate(value: any, {path}: {path: string} = {path: 'Value'}) {
+  validate(value: any, {path}: ConfigValidate = {path: 'Value'}) {
     for (const testEntity of this.context.rules) {
       const error = testEntity.test(value, {path});
       if (error !== '') {
@@ -22,6 +23,16 @@ export abstract class Schema<TValue> {
       }
     }
     return '';
+  }
+
+  validateObj(value: any, config: ConfigValidate) {
+    const validateError = this.validate(value, config);
+
+    if (validateError !== '') {
+      return validateError;
+    }
+
+    return value as Infer<typeof this>;
   }
 
   test<TValue extends (typeof this)['types']>(cb: Test) {
